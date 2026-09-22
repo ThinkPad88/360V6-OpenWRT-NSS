0f2086023bba0db18106f2c6baa0391aa8dd7976
ThinkPad88 <95933091+ThinkPad88@users.noreply.github.com>
2026-09-22 15:01:59 +0800

    Update dashboard.lua

--- packages/luci-app-dashboard/files/usr/share/luci/controller/dashboard.lua
+++ packages/luci-app-dashboard/files/usr/share/luci/controller/dashboard.lua
@@ -1,5 +1,5 @@
 module("luci.controller.dashboard", package.seeall)
 
 function index()
-    entry({"admin", "dashboard"}, template("dashboard/index"), _("ä»ªè¡¨ç"), 1)
+    entry({"admin", "dashboard"}, template("dashboard/index"), _("Dashboard"), 1)
 end
